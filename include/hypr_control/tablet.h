48d8503a55065ab39e316a6394eec25175b8bd9a
Branden Call <brandencall@live.com>
2025-09-22 13:55:54 -0600

    kill and launch waybar. left-up, right-up, normal state init setup

--- include/hypr_control/tablet.h
+++ include/hypr_control/tablet.h
@@ -7,3 +7,7 @@ void handle_tablet_event(const std::string &event);
 void enter_tablet_mode();
 void exit_tablet_mode();
 void execute_script(std::filesystem::path script);
+void kill_waybar();
+void launch_waybar();
+void execute_script(std::filesystem::path path, std::string cmd, std::string process);
+void execute_script(std::filesystem::path path, std::string cmd);
--- src/hypr_control/tablet.cpp
+++ src/hypr_control/tablet.cpp
@@ -1,12 +1,23 @@
 #include "hypr_control/tablet.h"
 #include "hypr_control/hypr_util.h"
 #include <iostream>
+#include <string>
+
+bool tablet_on = false;
 
 void handle_tablet_event(const std::string &event) {
     if (event == "tablet_on") {
         enter_tablet_mode();
+        tablet_on = true;
     } else if (event == "tablet_off") {
         exit_tablet_mode();
+        tablet_on = false;
+    } else if (event == "left-up" && tablet_on) {
+        std::cout << "handle left up" << '\n';
+    } else if (event == "right-up" && tablet_on) {
+        std::cout << "handle right up" << '\n';
+    } else if (event == "normal" && tablet_on) {
+        std::cout << "handle normal" << '\n';
     }
 }
 
@@ -17,8 +28,13 @@ void enter_tablet_mode() {
         std::cout << "there is no home directory" << '\n';
         return;
     }
+
     std::filesystem::path script = std::filesystem::path(home) / ".config/eww/launch_dashboard.sh";
     execute_script(script);
+    std::filesystem::path kill_script = "/usr/bin/killall";
+    std::string kill_cmd = "killall";
+    std::string waybar = "waybar";
+    execute_script(kill_script, kill_cmd, waybar);
 }
 
 void exit_tablet_mode() {
@@ -29,11 +45,32 @@ void exit_tablet_mode() {
     }
     std::filesystem::path script = std::filesystem::path(home) / ".config/eww/close_dashboard.sh";
     execute_script(script);
+    std::filesystem::path waybar_script = "/usr/bin/waybar";
+    std::string waybar = "waybar";
+    execute_script(waybar_script, waybar);
 }
 
 void execute_script(std::filesystem::path script) {
     if (fork() == 0) {
         execl(script.c_str(), script.c_str(), (char *)nullptr);
+        _exit(127);
+    }
+}
+
+void kill_waybar() {
+    std::filesystem::path kill_script = "/usr/bin/killall";
+    std::string kill_cmd = "killall";
+    std::string waybar = "waybar";
+    if (fork() == 0) {
+        execl(kill_script.c_str(), kill_cmd.c_str(), waybar.c_str(), (char *)nullptr);
+        _exit(127);
+    }
+}
+void launch_waybar() {
+    std::filesystem::path waybar_script = "/usr/bin/waybar";
+    std::string waybar = "waybar";
+    if (fork() == 0) {
+        execl(waybar_script.c_str(), waybar.c_str(), (char *)nullptr);
         _exit(127);
     }
 }
